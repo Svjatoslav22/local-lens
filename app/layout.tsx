d1484af80b30dc785a8516472f2f089cb608776f
index22 <116450399+Svjatoslav22@users.noreply.github.com>
2025-11-12 15:52:18 +0200

    og-image для соціальних прев'ю

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,22 +12,26 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-    openGraph: {
-    title: "Next.js SEO Guide",
-    description: "Як покращити SEO вашого сайту",
+  title: "Local Lens Sambir",
+  description: "Відкрийте найкращі місця Самбора: ресторани, кафе, спортивні центри, парки та розваги.",
+  keywords: ["Самбір", "місця Самбора", "ресторани", "кафе", "парки", "розваги"],
+  openGraph: {
+    title: "Local Lens Sambir ",
+    description: "Відкрийте найкращі місця Самбора: ресторани, кафе, спортивні центри, парки та розваги.",
     url: "https://local-lens-sambir-ss.vercel.app",
-    siteName: "Мій сайт",
+    siteName: "Local Lens Sambir",
     images: [
       {
-        url: "https://www.mindinventory.com/blog/wp-content/uploads/2022/10/City-Guide-App-for-Travelers1200.png",
-        width: 800,
-        height: 600,
-        alt: "Next.js SEO Preview",
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Local Lens Sambir - Карта локацій",
       },
     ],
     locale: "uk_UA",
     type: "website",
   },
+  
 };
 
 export default function RootLayout({
